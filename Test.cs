be2df2e8fd08e06bb95a8a3b9c0c47fbb5cfa004
Nicola <nicola_rushton@outlook.com>
2022-03-25 10:08:03 +0000

    tried a test

---
+++ Test.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Codurance.FunctionAPITest {
+public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
